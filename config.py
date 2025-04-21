4aa68ea0858866dbe0ace380d00b8a7a627c2560
Chinna op <opchinna742@gmail.com>
2025-04-21 18:21:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "28362850")) #optional
 API_HASH = getenv("API_HASH", "34f9cb93364db16fc45d003e4c81d97a") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7588442909").split()))
-OWNER_ID = int(getenv("OWNER_ID", "7635867946"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8187405882").split()))
+OWNER_ID = int(getenv("OWNER_ID", "8187405882"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-BOT_TOKEN = getenv("BOT_TOKEN", "7660309192:AAHsDRuxVOj1p-55QKPm8bnU6qQ515u0loI")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/w04ren.jpg')
+BOT_TOKEN = getenv("BOT_TOKEN", "7500951257:AAFwz8XYQ_bFBjiqcFeE9yN0QfU3-Kl6gik")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://graph.org/file/6d78e1e8de6339de61236-35d9fdfe123743c20f.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP", "1002141779241")
+LOG_GROUP = getenv("LOG_GROUP", "-1002532559194")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/GDNBharat/Toxic-userbot")
 BRANCH = getenv("BRANCH", "main") #don't change
